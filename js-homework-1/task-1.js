1745a246ac4d9b99c2988c5c5ad67e198f5fad29
Алексей Савельев <mtptz21@gmail.com>
2022-06-06 17:08:20 +0300

    js-hw1-task1

---
+++ js-homework-1/task-1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const nameproduct = 'Генератор защитного поля';
+let price = 1000;
+console.log(`Выбран ${nameproduct}, цена за штуку ${price} кредитов`);
+
+price = 2000;
+console.log(`Выбран ${nameproduct}, цена за штуку ${price} кредитов`);
